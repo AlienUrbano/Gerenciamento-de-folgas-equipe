4b9810bc6ccdc11c3ada91566435c0858deea66e
Paulo Gabriel Neves Santos <Paulogabrielneves@hotmail.com>
2023-07-23 04:12:26 -0300

    Dando forma ao front-end

---
+++ calendario-mananger/script.js
@@ -0,0 +1,13 @@
+function mostrarConteudo(conteudoId) {
+   
+    const conteudos = document.querySelectorAll(".conteudo");
+    for (const conteudo of conteudos) {
+        conteudo.style.display = "none";
+    }
+
+    
+    const conteudoSelecionado = document.getElementById(conteudoId);
+    conteudoSelecionado.style.display = "block";
+
+ 
+}
