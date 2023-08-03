47e4dfe37fb83f05ed837c2d4a8508e1fd84ed6b
Paulo Gabriel Neves Santos <Paulogabrielneves@hotmail.com>
2023-08-03 10:23:41 -0300

    update

---
+++ calendario-mananger/dayoff.js
@@ -0,0 +1,9 @@
+const modalOverlay = document.getElementById("modalOverlay");
+
+function openModal() {
+  modalOverlay.classList.add("modal-visible");
+}
+
+function closeModal() {
+  modalOverlay.classList.remove("modal-visible");
+}
