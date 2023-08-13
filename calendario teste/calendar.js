18730062e9b949abc3717becdd40c3a93cb7be35
Paulo Gabriel Neves Santos <Paulogabrielneves@hotmail.com>
2023-08-13 00:59:53 -0300

    Update calendar.js

--- calendario teste/calendar.js
+++ calendario teste/calendar.js
@@ -1,6 +1,3 @@
-console.log("carregoou!");
-
-
 const divCalendar = document.getElementById("calendar")
 
 const h1 = document.createElement("h1")
@@ -11,9 +8,43 @@ const table = document.createElement('table')
 const thead = document.createElement('thead')
 const tr = document.createElement('tr')
 const td = document.createElement('td')
-td.innerText='domingo'
-table.appendChild(thead)
+
+const tbody = document.createElement('tbody')
+
+
+table.appendChild(tbody)
 thead.appendChild(tr)
 tr.appendChild(td)
 
+
+    function imprimeDiasDaSemanas() {
+        const dias  = ["DOM","SEG","TER","QUA","QUI","SEX","SAB"]
+        const tr = document.createElement('tr')
+        
+        for(const dia of dias){
+            const td = document.createElement("td")
+            td.innerText = dia
+            tr.appendChild(td)
+        }
+        tbody.appendChild(tr)
+    }
+
+    function imprimeDiasNumericos() {
+        let tr = document.createElement("tr")
+        for(let dia =1; dia <=31; dia++){
+            const td = document.createElement("td")
+            td.innerText = dia
+            tr.appendChild(td)
+
+            if (dia % 7 === 0) {
+                tbody.appendChild(tr)
+                tr = document.createElement("tr")
+            }
+        }
+        tbody.appendChild(tr)
+    }
+
+
+imprimeDiasDaSemanas()
+imprimeDiasNumericos()
 divCalendar.appendChild(table)
